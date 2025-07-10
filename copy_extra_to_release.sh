b20535f74befa51c390c6c1d05c7ee872c6c1627
iluvatar1 <iluvatar1@users.noreply.github.com>
2025-07-10 21:34:37 +0000

    deploy: 6ca8c277df4704ced45588eb32d99962d28e6257

--- copy_extra_to_release.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-RELEASE_DIR=../release
-
-for myname in .devcontainer .github .gitignore _static Dockerfile README.md requirements.txt _config.yml _toc.yml; do
-    rsync -arv $myname $RELEASE_DIR/
-done
